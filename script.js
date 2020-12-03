e94c0e99d25d19927ec08affb8f12f07374422ed
Greg <gkodrucz@gmail.com>
2020-12-03 12:32:26 +0000

    thursday

--- script.js
+++ script.js
@@ -1,37 +1,39 @@
 function _load(){
-
-  document.getElementById("root").innerHTML = "<h1>wőrld</h1>";
-  document.getElementById("root").innerHTML += "<h2>európa</h2>";
-
-  document.getElementById("root").insertAdjacentHTML("beforeend", "<h3>wow,text</h3>");
-
-  document.getElementById("root").insertAdjacentHTML("beforeend", `
+  
+  let element = document.getElementById("main");
+  
+  element.innerHTML = "<h1>wőrld</h1>";
+  element.innerHTML += "<h2>európa</h2>";
+  
+  element.insertAdjacentHTML("beforeend", "<h3>wow,text</h3>");
+  
+  element.insertAdjacentHTML("beforeend", `
     <p>
       Lorem ipsum dolor sit amet consectetur adipisicing elit. Id laudantium recusandae iure voluptate et illo magni? Error, quod voluptas? Optio quas saepe, explicabo mollitia odit quod cumque placeat iste debitis.
     </p>
   `);
 
   //Ezzel az egesz elemet kitoroljuk vagy felul irjuk, sajat magat is beleertve.
-  //document.getElementById("root").outerHTML = "";
+  //element.outerHTML = "";
   
   //Ezt sem erdemes hasznalni, a css-nek a css-ben a helye.
-  //document.getElementById("root").style.padding = "80px";
+  //element.style.padding = "80px";
 
   //Ezt nem erdemes hasznalni
-  //document.getElementById("root").className = "rootClass";
-  //document.getElementById("root").className += " welcomeClass";
+  //element.className = "rootClass";
+  //element.className += " welcomeClass";
 
-  document.getElementById("root").classList.add("rootClass");
-  document.getElementById("root").classList.add("welcomeClass");
-  document.getElementById("root").classList.add("tempClass");
+  element.classList.add("rootClass");
+  element.classList.add("welcomeClass");
+  element.classList.add("tempClass");
 
-  document.getElementById("root").classList.remove("tempClass");  
+  element.classList.remove("tempClass");  
 
   
 }
 
 //ez igy nem fog betoltodni.
-//document.getElementById("root").innerHTML = "<h1>wőrld</h1>";
+//element.innerHTML = "<h1>wőrld</h1>";
 
 //igy sem erdemes csinalni
 //window.onload = _load();
